9dce3c1df53797f2f74d7faf7023ba34934056cd
Abhishek Kumar Shukla <as841840@student.nitw.ac.in>
2021-06-21 09:42:38 +0530

    feature importance for dataset comparison

--- src/components/FeatureImpDatasetComparison.js
+++ src/components/FeatureImpDatasetComparison.js
@@ -1,63 +1,84 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import Box from '@material-ui/core/Box';
+import GeneralTable from '../comparisonComps/GeneralTable';
 
-function createData(column, score) {
-    return { column, score };
+function createData(column, dataset) {
+    return { column, ...dataset };
 }
 
 export default function FeatureImpDatasetComparison(props){
     const evalList = props.evalList;
-    // console.log(evalList);
+
     const len = evalList.length;
-    // console.log(len);
+    const rows = [];
+    const headCells = [];
     const z = [];
     const x = [];
     const y = [];
     const trace = [];
+    headCells.push({id:'column', label:'Columns'});
     for(let i=0;i<len;i++){
-        // console.log(evalList[i].data.dataset.name);
+        console.log(evalList[i].data.metadata.feature_scores);
         z.push(evalList[i].data.dataset.name);
         y.push([]);
         x.push([]);
-    }
-
-    for(let i=0;i<len;i++){
+        headCells.push({id:i, label:z[i]});
         for(let j=0;j<evalList[i].data.metadata.feature_scores.length;j++){
             y[i].push(evalList[i].data.metadata.feature_scores[j]);
             x[i].push(evalList[i].data.dataset.metadata.columns[j]);
         }
-
         trace.push({
             x:x[i],
             y:y[i],
-            z:z[i],
-            type:'scatter3d',
-            mode:'lines'
+            type:'scatter',
+            mode:'lines',
+            name:z[i]
         });
     }
 
+    for(let i=0;i<evalList[0].data.metadata.feature_scores.length;i++){
+        let temp_feat_scores = [];
+        for(let j=0;j<len;j++){
+            temp_feat_scores.push(evalList[j].data.metadata.feature_scores[i].toFixed(2));
+        }
+        rows.push(createData(
+            evalList[0].data.dataset.metadata.columns[i],
+            temp_feat_scores
+        ))
+    }
+
     let data = [...trace];
-    console.log(trace);
-    // console.log(x,y,z);
+
     return(
     <div>
         <div className="row">
 
             <div className="col">
-                <Plot
-                    data={data}
-                    layout={{
-                        width:600,
-                        height:600,
-                        title:"Feature Importances"
-                    }}
-                />
+                <Box m={2} pr={10}>
+                    <Plot
+                        data={data}
+                        layout={{
+                            width:600,
+                            height:600,
+                            title:"Feature Importances"
+                        }}
+                        config={{
+                            scrollZoom:true,
+                            responsive:true
+                        }}
+                    />
+                </Box>
+                
             </div>
 
             <div className="col">
                 <Box m={2} pr={10}>
-
+                    <GeneralTable 
+                        headCells={headCells}
+                        rows={rows}
+                        tabletitle="Feature Importances"
+                    />
                 </Box>
             </div>
 
