03da9b9aac6305a59dc7f6b8c6b50444664f822f
Abhishek Kumar Shukla <as841840@student.nitw.ac.in>
2021-06-29 21:23:30 +0530

    Made UI better

--- client/src/components/PatchMetrics.js
+++ client/src/components/PatchMetrics.js
@@ -16,69 +16,70 @@ import UpdateMetricsForm from './UpdateMetricsForm';
 import AddMetricsForm from './AddMetricsForm';
 
 const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
 }))(TableCell);
 
 
 
 const useStyles = makeStyles({
-  table: {
-      minWidth: 400,
-      margin: "auto",
-  },
-  plot: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '90%',
-      height: '80%',
-  },
-  root: {
-    backgroundColor: 'rgb(219, 219, 219)',
-  },
-  gridContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    table: {
+        minWidth: 400,
+        margin: "auto",
+    },
+    plot: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        height: '80%',
+    },
+    root: {
+        backgroundColor: 'rgb(219, 219, 219)',
+    },
+    gridContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 
 function createData(metric, value) {
-  return { metric, value };
+    return { metric, value };
 }
-function pushAll(metric, value, rows, x, y) {
-  rows.push(createData(metric,value));
-  y.push(value);
-  x.push(metric);
+function pushAll(metric, value, rows) {
+    rows.push(createData(metric,value));
 }
 
 export default function Metrics(props){
     const rows = [];
-    const x =[];
-    const y= [];
-    const x_two=[];
-    const y_two=[];
     if(props.model_type==="regression") {
-        pushAll("MAE",props.metadata.mean_absolute_error.toFixed(2), rows, x, y);
-        pushAll("MSE",props.metadata.mean_squared_error.toFixed(2), rows, x, y);
-        pushAll("RMSE",props.metadata.root_mean_squared_error.toFixed(2), rows, x, y);
-        pushAll("RMSLE",props.metadata.root_mean_squared_log_error.toFixed(2), rows, x_two, y_two);
-        pushAll("R^2",props.metadata.Coefficient_of_Determination.toFixed(2), rows, x_two, y_two);
-        pushAll("Adjusted R^2",props.metadata.Adjusted_r_squared.toFixed(2), rows, x_two, y_two);
+        pushAll("MAE",props.metadata.mean_absolute_error.toFixed(2), rows);
+        pushAll("MSE",props.metadata.mean_squared_error.toFixed(2), rows);
+        pushAll("RMSE",props.metadata.root_mean_squared_error.toFixed(2), rows);
+        pushAll("RMSLE",props.metadata.root_mean_squared_log_error.toFixed(2), rows);
+        pushAll("R^2",props.metadata.Coefficient_of_Determination.toFixed(2), rows);
+        pushAll("Adjusted R^2",props.metadata.Adjusted_r_squared.toFixed(2), rows);
 
     }
     else {
-        pushAll("Accuracy",props.metadata.accuracy_score.toFixed(2), rows, x, y);
-        pushAll("Precision Score",props.metadata.precision_score.toFixed(2), rows, x, y);
-        pushAll("Recall",props.metadata.recall.toFixed(2), rows, x, y);
-        pushAll("F1-Score",props.metadata.f1_score.toFixed(2), rows, x, y);
-        pushAll("Log-Loss",props.metadata.log_loss.toFixed(2), rows, x, y);
+        pushAll("Accuracy",props.metadata.accuracy_score.toFixed(2), rows);
+        pushAll("Precision Score",props.metadata.precision_score.toFixed(2), rows);
+        pushAll("Recall",props.metadata.recall.toFixed(2), rows);
+        pushAll("F1-Score",props.metadata.f1_score.toFixed(2), rows);
+        pushAll("Log-Loss",props.metadata.log_loss.toFixed(2), rows);
     }
+
+    const other_metrics_rows = [];
+    console.log(props.metadata.additional_metrics);
+    for(let [key, value] of Object.entries(props.metadata.additional_metrics)){
+        pushAll(key, value, other_metrics_rows);
+    }
+    
 
     const [open, setOpen] = React.useState(false);
     const [addOpen, setAddOpen] = React.useState(false);
@@ -96,7 +97,7 @@ export default function Metrics(props){
         setAddOpen(false);
     };
     return(
-        <Grid container xs={12} className={classes.root}>
+        <Grid container spacing={2} className={classes.root}>
             <Grid item xs={12}>
                 <Box mb={2} >
                     <Paper elevation={5}>
@@ -111,36 +112,11 @@ export default function Metrics(props){
                     </Paper> 
                 </Box>
             </Grid>
-            <Grid container spacing={2}>
-            
-                <Grid item xs={12} sm={6}>
-                    <Paper elevation={5}>
-                        <TableContainer  component={Paper}>
-                            <Table className={classes.table} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <StyledTableCell>Metrics</StyledTableCell>
-                                        <StyledTableCell align="right">Score</StyledTableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                {rows.map((row) => (
-                                    <TableRow key={row.name}>
-                                        <StyledTableCell component="th" scope="row">
-                                            {row.metric}
-                                        </StyledTableCell>
-                                    <StyledTableCell align="right">{row.value}</StyledTableCell>
-                                    </TableRow>
-                                ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Paper>
-                    
-                </Grid>
-                <Grid container xs={12} sm={6}>
-                    <Grid item xs={6} sm={3}>
+            <Grid item xs={12}>
+                <Grid container spacing={2} className={classes.gridContainer}>
+                    <Grid item xs={12} sm={3}>
                         <Button 
+                            fullWidth
                             color="primary" 
                             variant="contained"
                             onClick={handleAddOpen}
@@ -154,8 +130,9 @@ export default function Metrics(props){
                             metadata={props.metadata}
                         />
                     </Grid>
-                    <Grid item xs={12} sm={6}>
+                    <Grid item xs={12} sm={3}>  
                         <Button 
+                            fullWidth
                             color="primary" 
                             variant="contained"
                             onClick={handleOpen}
@@ -169,6 +146,87 @@ export default function Metrics(props){
                             metadata={props.metadata}
                         />
                     </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container spacing={2} className={classes.gridContainer}>
+                    {props.metadata.additional_metrics.length === 0 ? (
+                        <Grid item xs={12} sm={6}>
+                            <Paper elevation={5}>
+                                <TableContainer  component={Paper}>
+                                    <Table className={classes.table} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <StyledTableCell align="center">Metrics</StyledTableCell>
+                                                <StyledTableCell align="center">Score</StyledTableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                        {rows.map((row) => (
+                                            <TableRow key={row.name}>
+                                                <StyledTableCell align="center" scope="row">
+                                                    {row.metric}
+                                                </StyledTableCell>
+                                            <StyledTableCell align="center">{row.value}</StyledTableCell>
+                                            </TableRow>
+                                        ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Paper>
+                        </Grid>
+                    ) : (
+                        <>
+                            <Grid item xs={12} sm={6}>
+                                <Paper elevation={5}>
+                                    <TableContainer  component={Paper}>
+                                        <Table className={classes.table} aria-label="simple table">
+                                            <TableHead>
+                                                <TableRow>
+                                                    <StyledTableCell align="center">Metrics</StyledTableCell>
+                                                    <StyledTableCell align="center">Score</StyledTableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                            {other_metrics_rows.map((row) => (
+                                                <TableRow key={row.name}>
+                                                    <StyledTableCell align="center" scope="row">
+                                                        {row.metric}
+                                                    </StyledTableCell>
+                                                <StyledTableCell align="center">{row.value}</StyledTableCell>
+                                                </TableRow>
+                                            ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                            <Paper elevation={5}>
+                                    <TableContainer  component={Paper}>
+                                        <Table className={classes.table} aria-label="simple table">
+                                            <TableHead>
+                                                <TableRow>
+                                                    <StyledTableCell align="center">Metrics</StyledTableCell>
+                                                    <StyledTableCell align="center">Value</StyledTableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                            {rows.map((row) => (
+                                                <TableRow key={row.name}>
+                                                    <StyledTableCell align="center" scope="row">
+                                                        {row.metric}
+                                                    </StyledTableCell>
+                                                <StyledTableCell align="center">{row.value}</StyledTableCell>
+                                                </TableRow>
+                                            ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Paper>
+                            </Grid>
+                        </>
+                    )}
                 </Grid>
             </Grid>
         </Grid>
