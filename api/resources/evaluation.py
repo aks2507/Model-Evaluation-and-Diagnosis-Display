3d5984d54a66e1b2cc0e45e0fc792529a2dd1a4d
Abhishek Kumar Shukla <as841840@student.nitw.ac.in>
2021-06-29 13:08:39 +0530

    patch request

--- api/resources/evaluation.py
+++ api/resources/evaluation.py
@@ -1,10 +1,72 @@
 from flask_restful import Resource, reqparse
+from db import db
 from models.evaluation import EvalModel
 from resources.eval_functions import EvaluationFunctions
 from models.datasets import Dataset
 from models.mlmodels import MLModel
 import logging
 class Evaluate(Resource):
+    regression_parser = reqparse.RequestParser()
+    classification_parser = reqparse.RequestParser()  
+    # Regression Parser
+    regression_parser.add_argument('mean_absolute_error',
+        type=float,
+        required=True,
+        help="Please provide Mean absolute score"
+    )
+    regression_parser.add_argument('mean_squared_error',
+        type=float,
+        required=True,
+        help="Please provide a mean squared error value"
+    )
+    regression_parser.add_argument('root_mean_squared_error',
+        type=float,
+        required=True,
+        help="Please provide a root mean squared error value"
+    )
+    regression_parser.add_argument('root_mean_squared_log_error',
+        type=float,
+        required=True,
+        help="Please provide a root mean squared logarithmic error value"
+    )
+    regression_parser.add_argument('Coefficient_of_Determination',
+        type=float,
+        required=True,
+        help="Please provide a r squared score"
+    )
+    regression_parser.add_argument('Adjusted_r_squared',
+        type=float,
+        required=True,
+        help="Please provide an adjusted r squared score"
+    )
+
+    # Classification parser
+    classification_parser.add_argument('accuracy_score',
+        type=float,
+        required=True,
+        help="Please provide Accuracy score"
+    )
+    classification_parser.add_argument('precision_score',
+        type=float,
+        required=True,
+        help="Please provide Precision score"
+    )
+    classification_parser.add_argument('f1_score',
+        type=float,
+        required=True,
+        help="Please provide an f1 score"
+    )
+    classification_parser.add_argument('recall',
+        type=float,
+        required=True,
+        help="Please provide a recall value"
+    )
+    classification_parser.add_argument('log_loss',
+        type=float,
+        required=True,
+        help="Please provide a log loss value"
+    )
+
     def get(self,eval_id):
         evaluation_entity = EvalModel.find_by_id(eval_id)
         if evaluation_entity:
@@ -33,6 +95,27 @@ class Evaluate(Resource):
         if evaluation_entity:
             evaluation_entity.delete_from_db()
         return {"message":"Evaluation removed"}
+    
+    def patch(self, eval_id):
+        evaluation_entity = EvalModel.query.filter_by(eval_id = eval_id).first()
+        update_entity = EvalModel.query.filter_by(eval_id = eval_id)
+        if evaluation_entity:
+            data=[]
+            
+            if evaluation_entity.model_type == "regression":
+                data = Evaluate.regression_parser.parse_args()
+            else:
+                data = Evaluate.classification_parser.parse_args()
+            
+            eval_dict = evaluation_entity.json()
+            for key, value in data.items():
+                if data[key] is not None:
+                    eval_dict["metadata"][key] = value
+            metrics = eval_dict["metadata"]
+            update_entity.update(dict(meta = metrics))
+            db.session.commit()
+            return {"message":"Patch request done"}, 201
+        return {"message":"Evaluation entity does not exist"}, 404
 
 
 class EvaluateList(Resource):
