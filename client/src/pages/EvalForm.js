3769c7bbb8fba9e5955a4f0e49ece2b23cbae514
Abhishek Kumar Shukla <as841840@student.nitw.ac.in>
2021-06-30 05:08:54 +0530

    evalform fixed

--- client/src/pages/EvalForm.js
+++ client/src/pages/EvalForm.js
@@ -10,78 +10,83 @@ import PostAddIcon from '@material-ui/icons/PostAdd';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
-import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
+import { Paper, Grid} from '@material-ui/core';
 
 import Navbar from '../components/Navbar';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
+	paper: {
+		display: 'block',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent:'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+	},
+	formElements: {
+		marginLeft: '8%', 
+		marginRight: '8%',
+	}
 }));
 
 export default function EvalForm(props) {
-  let datasets = props.datasets;
-  let models = props.models;
-  console.log(datasets);
-  const classes = useStyles();
-  const [openDS, setOpenDS] = React.useState(false);
-  const [openModel, setOpenModel] = React.useState(false);
-  const [modelID, setModelID] = React.useState(0);
-  const [datasetID, setDatasetID] = React.useState(0);
+	let datasets = props.datasets;
+	let models = props.models;
+	console.log(datasets);
+	const classes = useStyles();
+	const [openDS, setOpenDS] = React.useState(false);
+	const [openModel, setOpenModel] = React.useState(false);
+	const [modelID, setModelID] = React.useState(0);
+	const [datasetID, setDatasetID] = React.useState(0);
 
-  const [values, setValues] = useState({
-        name: 'test_eval',
-        model_type: 'regression',
-        model_id: 0,
-        dataset_id: 0,
-        metadata: {},
-  });
+	const [values, setValues] = useState({
+		name: 'test_eval',
+		model_type: 'regression',
+		model_id: 0,
+		dataset_id: 0,
+		metadata: {},
+	});
 
-  const handleDropdownChangeModelID = (event) => {
-    setModelID(event.target.value);
-    setValues({ ...values, 'model_id': event.target.value });
-  };
+	const handleDropdownChangeModelID = (event) => {
+		setModelID(event.target.value);
+		setValues({ ...values, 'model_id': event.target.value });
+	};
 
-  const handleDropdownChangeDatasetID = (event) => {
-    setDatasetID(event.target.value);
-    setValues({ ...values, 'dataset_id': event.target.value });
-  };
+	const handleDropdownChangeDatasetID = (event) => {
+		setDatasetID(event.target.value);
+		setValues({ ...values, 'dataset_id': event.target.value });
+	};
 
-  const handleModelClose = () => {
-    setOpenModel(false);
-  };
+	const handleModelClose = () => {
+		setOpenModel(false);
+	};
 
-  const handleModelOpen = () => {
-    setOpenModel(true);
-  };
+	const handleModelOpen = () => {
+		setOpenModel(true);
+	};
 
-  const handleDatasetClose = () => {
-    setOpenDS(false);
-  };
+	const handleDatasetClose = () => {
+		setOpenDS(false);
+	};
 
-  const handleDatasetOpen = () => {
-    setOpenDS(true);
-  };
+	const handleDatasetOpen = () => {
+		setOpenDS(true);
+	};
 
 
     const handleSubmit = async (e) => {
@@ -101,114 +106,126 @@ export default function EvalForm(props) {
     };
 
   return (
-    <>
-      <Navbar />
-          <Box ml={36} mr={36} pr={15} pl={15}>
-            <Paper elevation={5}>
-              <Box m={2} pl={3} pr={3} pb={3}>
-          <CssBaseline />
-          <div className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <PostAddIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Add Evaluation
-            </Typography>
-            <form className={classes.form} onSubmit={handleSubmit}>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="Evaluation Name"
-                name="name"
-                autoComplete="Evaluation Name"
-                autoFocus
-                onChange={handleChange('name')}
-              />
+	<Grid container spacing={5} style={{alignItems:'center', justifyContent:'center',}}>
+		<Grid item xs={12}>
+			<Navbar />
+		</Grid>
+		<Grid item xs={8} sm={4} style={{alignItems:'center', justifyContent:'center',}}>
+			<Paper elevation={5} >
+			<CssBaseline />
+				<Grid container spacing={2} className={classes.paper}>
+					<Grid item align="center" xs={12} sm={12} lg={12} style={{alignItems:'center', justifyContent:'center',}}>
+						<Avatar className={classes.avatar} align="center">
+							<PostAddIcon />
+						</Avatar>
+					</Grid>
+					<Grid item xs={12} sm={12} lg={12} style={{alignItems:'center', justifyContent:'center',}}>
+						<Typography component="h1" variant="h5" align="center">
+							Add Evaluation
+						</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<form className={classes.form} onSubmit={handleSubmit}>
+							<Grid container spacing={2}>
+								<Grid item xs={12} sm={12} lg={12} align="center" className={classes.formElements}>
+									<TextField
+										variant="outlined"
+										margin="none"
+										required
+										fullWidth
+										id="name"
+										label="Evaluation Name"
+										name="name"
+										autoComplete="Evaluation Name"
+										autoFocus
+										onChange={handleChange('name')}
+									/>
+								</Grid>
+								<Grid item xs={12} sm={12} lg={12} className={classes.formElements}>
+									<div className={classes.formControl}>
+										<InputLabel id="dataset_id">Dataset</InputLabel>
+										<Select
+											labelId="dataset_id"
+											id="did"
+											open={openDS}
+											fullWidth
+											onClose={handleDatasetClose}
+											onOpen={handleDatasetOpen}
+											value={datasetID}
+											onChange={handleDropdownChangeDatasetID}
+										>
+											{datasets.map((dataset) =>
+												<MenuItem value={dataset.dataset_id}>{dataset.name}</MenuItem>
+											)}
+										</Select>
+									</div>
+								</Grid>
+								<Grid item xs={12} sm={12} lg={12} className={classes.formElements}>
+									<div className={classes.formControl}>
+										<InputLabel id="model_id">Model</InputLabel>
+										<Select
+											labelId="model_id"
+											id="mid"
+											fullWidth
+											open={openModel}
+											onClose={handleModelClose}
+											onOpen={handleModelOpen}
+											value={modelID}
+											onChange={handleDropdownChangeModelID}
+										>
+											{models.map((model) =>
+												<MenuItem value={model.model_id}>{model.name}</MenuItem>
+											)}
+										</Select>
+									</div>
+								</Grid>
+								<Grid item xs={12} sm={12} lg={12} align="center" className={classes.formElements}>
+									<TextField
+										variant="filled"
+										margin="normal"
+										fullWidth
+										name="description"
+										label="Description"
+										id="description"
+										autoComplete="Evaluation Description"
+									/>
+								</Grid>
+								<Grid item xs={12} sm={12} lg={12} align="center" className={classes.formElements}>
+									<Grid className="row">
+										<Grid className="col">
+											<Button
+											type="submit"
+											fullWidth
+											variant="contained"
+											color="secondary"
+											size="large"
+											className={classes.submit}
+											onClick={handleSubmit}
+											>
+											Add
+											</Button>
+										</Grid>
 
-              <div className={classes.formControl}>
-                <InputLabel id="dataset_id">Dataset</InputLabel>
-                <Select
-                  labelId="dataset_id"
-                  id="did"
-                  open={openDS}
-                  fullWidth
-                  onClose={handleDatasetClose}
-                  onOpen={handleDatasetOpen}
-                  value={datasetID}
-                  onChange={handleDropdownChangeDatasetID}
-                >
-                  {datasets.map((dataset) =>
-                    <MenuItem value={dataset.dataset_id}>{dataset.name}</MenuItem>
-                  )}
-                </Select>
-              </div>
-
-              <div className={classes.formControl}>
-                <InputLabel id="model_id">Model</InputLabel>
-                <Select
-                  labelId="model_id"
-                  id="mid"
-                  fullWidth
-                  open={openModel}
-                  onClose={handleModelClose}
-                  onOpen={handleModelOpen}
-                  value={modelID}
-                  onChange={handleDropdownChangeModelID}
-                >
-                  {models.map((model) =>
-                    <MenuItem value={model.model_id}>{model.name}</MenuItem>
-                  )}
-                </Select>
-              </div>
-
-              <TextField
-                variant="filled"
-                margin="normal"
-                fullWidth
-                name="description"
-                label="Description"
-                id="description"
-                autoComplete="Evaluation Description"
-              />
-
-              <div className="row">
-
-                  <div className="col">
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      color="secondary"
-                      size="large"
-                      className={classes.submit}
-                      onClick={handleSubmit}
-                    >
-                      Add
-                    </Button>
-                  </div>
-
-                  <div className="col">
-                    <Button
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      size="large"
-                      className={classes.submit}
-                      onClick={handleCancel}
-                    >
-                      Cancel
-                    </Button>
-                  </div>
-
-                </div>
-              </form>
-            </div>
-            </Box>
-            </Paper>
-            </Box>
-    </>
+										<Grid className="col">
+											<Button
+											fullWidth
+											variant="contained"
+											color="primary"
+											size="large"
+											className={classes.submit}
+											onClick={handleCancel}
+											>
+											Cancel
+											</Button>
+										</Grid>
+									</Grid>
+								</Grid>
+							</Grid>
+						</form>
+					</Grid>
+				</Grid>
+			</Paper>
+		</Grid>
+	</Grid>
   );
 };
